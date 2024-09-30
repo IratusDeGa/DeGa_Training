4b035168b10af1e937893e3502a8bc2525026c3f
IratusDeGa <denisgalyanov@gmail.com>
2024-09-30 21:32:11 +0300

    Добавлен проект ДЗ1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Запрос фамилии
+        Console.Write("Введите фамилию: ");
+        string lastName = Console.ReadLine();
+
+        // Запрос имени
+        Console.Write("Введите имя: ");
+        string firstName = Console.ReadLine();
+
+        // Запрос отчества
+        Console.Write("Введите отчество: ");
+        string patronymic = Console.ReadLine();
+
+        // Отображение ФИО в одну строку
+        Console.WriteLine($"{lastName} {firstName} {patronymic}");
+    }
+}
